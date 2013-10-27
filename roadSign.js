3745008ed74cbe6d202cc4b436b63f5df3a8f047
abbystevens <adeats@gmail.com>
2013-10-27 14:48:20 -0700

    PCS assignment 5

---
+++ roadSign.js
@@ -0,0 +1,25 @@
+/*## Basic assignment 1
+
+Use the class creation pattern to implement a RoadSign class. It should have text, color and shape properties. 
+Use the constructor to create a Yield Sign, a Stop Sign and a Speed Limit Sign.
+
+upload your work as roadsign.js
+*/
+
+function RoadSign (text, color, shape) {
+	this.text = text;
+	this.color = color;
+	this.shape = shape;
+}
+
+RoadSign.prototype.whatsYourSign = function () {
+	console.log("I am a " + this.text + " sign.  I am " + this.color + " and I am a shapely " + this.shape + ".")
+}
+
+var yield = new RoadSign ("Yield", "red and white", "triangle");
+var stop = new RoadSign ("Stop", "red and white", "octogon");
+var speed = new RoadSign ("Speed Limit", "black and white", "square");
+
+console.log(yield.whatsYourSign());
+console.log(stop.whatsYourSign());
+console.log(speed.whatsYourSign());
